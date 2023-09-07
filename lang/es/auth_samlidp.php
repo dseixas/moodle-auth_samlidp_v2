7671cc3dfdfd61d4422cd215c9191b672dfc4c6f
Daniel Seixas <daniel_seixas@stratestys-ts.com>
2023-09-07 11:33:07 +0200

    Initial commit working

---
+++ lang/es/auth_samlidp.php
@@ -0,0 +1,85 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'auth_samlidp', language 'es'.
+ *
+ * @package    auth_samlidp_v2
+ * @copyright  2023 Daniel Seixas
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Proveedor de Identidad SAML';
+$string['auth_samlidpdescription'] = 'Esta es la configuración de un sitio de autenticación de Proveedor de Identidad';
+$string['coderoot_key'] = 'Directorio de instalación de SimpleSAMLphp';
+$string['coderoot_help'] = 'Ruta absoluta';
+$string['authsource_key'] = 'Fuente de autenticación';
+$string['authsource_help'] = 'Configurado en SimpleSAMLphp en config/authsources.php';
+
+$string['privacy:metadata:externalpurpose'] = 'El complemento Proveedor de Identidad SAML autentica a un usuario en Moodle y envía campos de perfil de usuario a un Proveedor de Servicios';
+$string['privacy:metadata:id'] = 'Id';
+$string['privacy:metadata:confirmed'] = 'Confirmado';
+$string['privacy:metadata:policyagreed'] = 'Acuerdo de política';
+$string['privacy:metadata:deleted'] = 'Eliminado';
+$string['privacy:metadata:suspended'] = 'Suspendido';
+$string['privacy:metadata:mnethostid'] = 'Mnethostid';
+$string['privacy:metadata:username'] = 'Nombre de usuario';
+$string['privacy:metadata:idnumber'] = 'Número de ID';
+$string['privacy:metadata:firstname'] = 'Nombre';
+$string['privacy:metadata:lastname'] = 'Apellido';
+$string['privacy:metadata:email'] = 'Correo electrónico';
+$string['privacy:metadata:emailstop'] = 'Detener correo electrónico';
+$string['privacy:metadata:icq'] = 'ICQ';
+$string['privacy:metadata:skype'] = 'Skype';
+$string['privacy:metadata:yahoo'] = 'Yahoo';
+$string['privacy:metadata:aim'] = 'AIM';
+$string['privacy:metadata:msn'] = 'MSN';
+$string['privacy:metadata:phone1'] = 'Teléfono 1';
+$string['privacy:metadata:phone2'] = 'Teléfono 2';
+$string['privacy:metadata:institution'] = 'Institución';
+$string['privacy:metadata:department'] = 'Departamento';
+$string['privacy:metadata:address'] = 'Dirección';
+$string['privacy:metadata:city'] = 'Ciudad';
+$string['privacy:metadata:country'] = 'País';
+$string['privacy:metadata:lang'] = 'Idioma';
+$string['privacy:metadata:calendartype'] = 'Tipo de calendario';
+$string['privacy:metadata:theme'] = 'Tema';
+$string['privacy:metadata:timezone'] = 'Zona horaria';
+$string['privacy:metadata:firstaccess'] = 'Primer acceso';
+$string['privacy:metadata:lastaccess'] = 'Último acceso';
+$string['privacy:metadata:lastlogin'] = 'Último inicio de sesión';
+$string['privacy:metadata:currentlogin'] = 'Inicio de sesión actual';
+$string['privacy:metadata:lastip'] = 'Última IP';
+$string['privacy:metadata:picture'] = 'Imagen';
+$string['privacy:metadata:url'] = 'URL';
+$string['privacy:metadata:description'] = 'Descripción';
+$string['privacy:metadata:descriptionformat'] = 'Formato de descripción';
+$string['privacy:metadata:mailformat'] = 'Formato de correo';
+$string['privacy:metadata:maildigest'] = 'Resumen de correo';
+$string['privacy:metadata:maildisplay'] = 'Visualización de correo';
+$string['privacy:metadata:autosubscribe'] = 'Autoinscripción';
+$string['privacy:metadata:trackforums'] = 'Seguimiento de foros';
+$string['privacy:metadata:timecreated'] = 'Hora de creación';
+$string['privacy:metadata:timemodified'] = 'Hora de modificación';
+$string['privacy:metadata:trustbitmask'] = 'Máscara de confianza';
+$string['privacy:metadata:imagealt'] = 'Texto alternativo de imagen';
+$string['privacy:metadata:lastnamephonetic'] = 'Apellido fonético';
+$string['privacy:metadata:firstnamephonetic'] = 'Nombre fonético';
+$string['privacy:metadata:middlename'] = 'Segundo nombre';
+$string['privacy:metadata:alternatename'] = 'Nombre alternativo';
+$string['privacy:metadata:profilefield'] = 'Campo de perfil personalizado';
---
+++ moodle/src/Auth/Source/External.php
@@ -0,0 +1,205 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * SimplSAMLphp module file
+ *
+ * * @package    auth_samlidp_v2
+ * @copyright  2023 Daniel Seixas
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+################################################################################
+/*
+ * In SimpleSAMLphp, in config/authsources.php, SIMILAR MUST BE ADDED:
+ *
+ *   'moodle-userpass' => array(
+ *       'moodle:External',
+ *       'moodle_coderoot' => '/var/www/ticket/moodle314/www',
+ *       'logout_url' => 'https://10.0.0.28/moodle314/auth/samlidp/logout.php',  // plugin's logout page
+ *       'login_url' => 'https://10.0.0.28/moodle314/login/index.php',           // standard Moodle login page
+ *       'cookie_name' => 'MoodleSAMLIDPSessionID',
+ *   ),
+ */
+################################################################################
+declare(strict_types=1);
+
+namespace SimpleSAML\Module\moodle\Auth\Source;
+
+use Exception;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\Auth;
+use SimpleSAML\Configuration;
+use SimpleSAML\Error;
+use SimpleSAML\HTTP\RunnableResponse;
+use SimpleSAML\Module;
+use SimpleSAML\Session;
+use SimpleSAML\Utils;
+use SimpleSAML\Utilities;
+
+class External extends Auth\Source {
+    const STATE_IDENT = 'moodle:External';
+    private $config;
+
+    public function __construct($info, $config) {
+        assert('is_array($info)');
+        assert('is_array($config)');
+
+        parent::__construct($info, $config);
+        if (!isset($config{'cookie_name'})) {
+            throw new \SimpleSAML\Error\Exception('Misconfiguration in authsources');    # in the moodle part in config/authsources.php there must be 'cookie_name' setting
+        }
+        $ssp_config = \SimpleSAML\Configuration::getInstance();
+        $config['cookie_path'] = $ssp_config->getValue('session.cookie.path');
+        $config['cookie_salt'] = $ssp_config->getValue('secretsalt');
+        $this->config = $config;
+    }
+
+    public function authenticate(array &$state): void {
+
+        $user = $this->getUser();
+        if ($user) {
+            # user authenticated, nothing to do
+            $state['Attributes'] = $user;
+            return;
+        } else {
+            # redirect to a login page
+            $state['moodle:AuthID'] = $this->authId;
+            $state_id = \SimpleSAML\Auth\State::saveState($state, self::STATE_IDENT);
+            $return_to = \SimpleSAML\Module::getModuleURL('moodle/resume.php', array('State' => $state_id));
+            $auth_page = $this->config['login_url'] . '?ReturnTo=' . $return_to;
+            //\SimpleSAML\Utilities::redirect($auth_page, array('ReturnTo' => $return_to));
+	    $httpUtils = new Utils\HTTP();
+          $httpUtils->redirectTrustedURL($auth_page, array('ReturnTo' => $return_to));;
+        }
+    }
+
+    public static function resume() {
+        if (!isset($_REQUEST['State'])) {
+            throw new SimpleSAML_Error_BadRequest('Missing "State" parameter.');
+        }
+        $state_id = (string)$_REQUEST['State'];
+
+        $state = \SimpleSAML\Auth\State::loadState($state_id, self::STATE_IDENT);
+        $source = \SimpleSAML\Auth\Source::getById($state['moodle:AuthID']);
+        if ($source === NULL) {
+            throw new \SimpleSAML\Error\Exception('Could not find authentication source with id ' . $state[self::AUTHID]);
+        }
+
+        if (! ($source instanceof self)) {
+            throw new \SimpleSAML\Error\Exception('Authentication source type changed.');
+        }
+
+        $user = $source->getUser($state);
+        if ($user === NULL) {
+            throw new \SimpleSAML\Error\Exception('User not authenticated after login page.');
+        }
+        $state['Attributes'] = $user;
+
+        \SimpleSAML\Auth\Source::completeAuth($state);
+        exit(); # should never reach here
+    }
+
+    private function getUser() {
+        $uid = 0;
+        if (isset($_COOKIE[$this->config['cookie_name']]) && $_COOKIE[$this->config['cookie_name']]) {
+            $str_cookie = $_COOKIE[$this->config['cookie_name']];
+            # cookie created by: "setcookie($cookieName{'cookie_name'}, hash_hmac('sha1', $salt.$account->uid, $salt).':'.$uid, 0, $sspConfig->getValue('session.cookie.path'));"
+            # in auth/samlidp/auth.php in Moodle
+            $arr_cookie = explode(':', $str_cookie);
+
+            if ((isset($arr_cookie[0]) && $arr_cookie[0])
+                && (isset($arr_cookie[1]) && $arr_cookie[1])
+            ) {
+                # make sure no one manipulated the hash or the uid in the cookie before we trust the uid
+                if (hash_hmac('sha1', $this->config['cookie_salt'].$arr_cookie[1], $this->config['cookie_salt']) == $arr_cookie[0]) {
+                    $uid = (int)$arr_cookie[1];
+                } else {
+                    throw new \SimpleSAML\Error\Exception('Cookie hash invalid.');
+                }
+            }
+        }
+
+        # our cookie must be removed here
+        if (isset($_COOKIE[$this->config['cookie_name']])) {
+            setcookie($this->config['cookie_name'], "", time() - 3600, $this->config['cookie_path']);
+        }
+
+        if ($uid) {
+            # bootstrap moodle
+            global $CFG, $DB;
+            define('CLI_SCRIPT', true);
+            define('WEB_CRON_EMULATED_CLI', 'defined');
+            $configphp = $this->config['moodle_coderoot']."/config.php";
+            $userlib = $this->config['moodle_coderoot']."/user/profile/lib.php";
+            if (file_exists($configphp)) {
+                require_once($configphp);
+            } else {
+                throw new \SimpleSAML\Error\Exception('Moodle app instantiation failure: cannot require()' . $configphp);
+            }
+
+            if (file_exists($userlib)) {
+                require_once($userlib);
+            } else {
+                throw new \SimpleSAML\Error\Exception('Moodle app instantiation failure: cannot require()' . $userlib);
+            }
+            # query for a user
+            $user = $DB->get_record('user', array('id' => $uid));
+            $profile_fields = profile_user_record( (int) $user->id, false);
+
+            # simplesaml is somehow expecting a very weird structure in attributes ($user)
+            # we also don't reveal user password, auth method, secret
+            if ($user) {
+                unset($user->auth);
+                unset($user->password);
+                unset($user->secret);
+                $user->uid = $user->id;     # i dont believe this is strictly necessary. just nice-to-have
+                foreach ((array)$user as $param => $value) {
+                    $userattr[$param] = array($value);
+                }
+                foreach ($profile_fields as $param => $value) {
+                    $userattr['profile_field_'.$param] = array($value);
+                }
+            }
+            return $userattr;
+        } else {
+            return NULL;
+        }
+        return NULL;
+    }
+
+    public function logout (array &$state):void {
+        if (!session_id()) {
+            session_start();
+        }
+
+        if (isset($_COOKIE[$this->config['cookie_name']])) {
+            setcookie($this->config['cookie_name'], "", time() - 3600, $this->config['cookie_path']);
+        }
+        session_destroy();
+
+        $logout_url = $this->config['logout_url'];
+        if (!empty($state['ReturnTo'])) {
+            $logout_url .= '?ReturnTo=' . $state['ReturnTo'];
+        }
+
+        //\SimpleSAML\Utilities::redirect($logout_url);
+	$httpUtils = new Utils\HTTP();
+        $httpUtils->redirectTrustedURL($logout_url);
+
+        die();
+    }
+}
